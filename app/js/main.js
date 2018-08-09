159cc633499fab8775ec138d038d88f1b09ebd5e
Doan Van Quang <quang99dev@gmail.com>
2018-08-09 23:02:53 +0700

    Full screen

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,31 @@ $range_load.on({
     'input': handleChangeRange
 });
 
+//Bottom menu
+var menuIndex = 2;
+showMenu(menuIndex);
+
+function plusSlides(n) {
+    showMenu(menuIndex += n);
+}
+
+function currentSlide(n) {
+    showMenu(menuIndex = n);
+}
+
+function showMenu(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var buttonItem = document.getElementsByClassName("material-icons");
+    if (n > slides.length) { menuIndex = 1 }
+    if (n < 1) { menuIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < buttonItem.length; i++) {
+        buttonItem[i].className = buttonItem[i].className.replace(" active", "");
+    }
+    slides[menuIndex - 1].style.display = "block";
+    buttonItem[menuIndex - 1].className += " active";
+}
+//Bottom menu
